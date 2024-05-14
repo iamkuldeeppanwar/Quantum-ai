f91ca61718bbdc241281852baa80b34cdd45af59
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2024-05-14 12:45:21 +0530

    fix sidenav

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { Store } from "./states/store";
-
-import { AdminProtectedRoute } from "./routes";
 import { Header, SideNavBar, NotFound } from "./components";
-import { AdminLoginScreen, Dashboard } from "./pages";
+import { Dashboard } from "./pages";
 import Objectdetect from "./pages/Object detection/Objectdetect";
 import ImageRecognize from "./pages/Image Recognize/ImageRecognize";
 import SpeechText from "./pages/Speech To Text/SpeechText";
 
 function App() {
-  const { state } = useContext(Store);
-  const { token } = state;
-
   const pageLocation = useLocation();
-
   const [isExpanded, setExpandState] = useState(window.innerWidth > 768);
   const sidebarHandler = () => setExpandState((prev) => !prev);
 
-  // console.log({ isExpanded, token });
   return (
     <div className="main-wrapper">
-      {isExpanded && token && (
+      {isExpanded && (
         <div className="sidebar-overlay" onClick={sidebarHandler}></div>
       )}
-      {token && (
-        <div className="sidebar-wrapper">
-          <SideNavBar isExpanded={isExpanded} />
-        </div>
-      )}
+
+      <div className="sidebar-wrapper">
+        <SideNavBar isExpanded={isExpanded} />
+      </div>
+
       <div
         className={`body-wrapper ${isExpanded ? "mini-body" : "full-body"} 
-        ${token ? "" : "m-0"} d-flex flex-column`}
+        d-flex flex-column`}
       >
         {/* {token && <Header sidebarHandler={sidebarHandler} />} */}
         <Routes location={pageLocation} key={pageLocation.pathname}>
@@ -41,7 +33,6 @@ function App() {
           <Route path="/Ai/speech-recognize" element={<SpeechText />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-        {/* <Footer /> */}
       </div>
     </div>
   );
